437dee8aaa489fce9409ee8e09513c5a158e7c35
Jonathan Kila <temidee11@yahoo.co.uk>
2023-03-23 08:16:30 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from "solid-js"
+import Header from "./components/Header/Header"
+
+const App: Component = () => {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import { Component } from "solid-js"
+
+const Header: Component = () => (
+  <header class="header">
+    <div class="header__container">
+      <div class="logo">LifeStyle</div>
+    </div>
+  </header>
+)
+
+export default Header
