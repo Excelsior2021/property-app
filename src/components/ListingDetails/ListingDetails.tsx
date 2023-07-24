2f122c702b4ec18a21191deebb984dada63f9158
Jonathan Kila <temidee11@yahoo.co.uk>
2023-07-24 19:32:38 +0100

    updates

--- src/components/ListingDetails/ListingDetails.tsx
+++ src/components/ListingDetails/ListingDetails.tsx
@@ -1,13 +1,14 @@
 import { Component, Show } from "solid-js"
 import { useNavigate } from "@solidjs/router"
 import { listingDataType } from "../../types/general"
-import ListingItemDetails from "../ListingItemDetails/ListingItemDetails"
 import { loggedIn, setModal, setModalOverlayData } from "../../store/store"
 import routes from "../../utils/client-routes"
 import "./ListingDetails.scss"
+import { handleSave } from "../../api/api"
 
 interface listingDetailsProps {
   listing: listingDataType
+  saved: boolean
 }
 
 const ListingDetailsContactButton = () => {
@@ -29,15 +30,40 @@ const ListingDetailsContactButton = () => {
 }
 
 const ListingDetails: Component<listingDetailsProps> = props => {
+  const navigate = useNavigate()
+
+  const handleClickOnSave = (listing: listingDataType) => {
+    if (loggedIn()) {
+      if (props.saved) handleSave(listing, false)
+      else handleSave(listing, true)
+    } else navigate(routes.login)
+  }
+
   return (
     <div class="listing-details">
-      <p class="listing-details__location">{props.listing.location}</p>
-      <p class="listing-details__price">Gh₵{props.listing.price} p/m</p>
+      <div class="listing-details__container listing-details__container--top">
+        <div>
+          <p class="listing-details__location">{props.listing.location}</p>
+          <p class="listing-details__price">Gh₵{props.listing.price} p/m</p>
+        </div>
+        <div
+          class="listing-details__container listing-details__container--save"
+          onclick={() => handleClickOnSave(props.listing)}>
+          <img
+            src={props.saved ? "/icons/saved-active.svg" : "/icons/saved.svg"}
+            alt="save listing"
+            class="listing-details__icon"
+          />
+          <p class="listing-details__icon-text">
+            {props.saved ? "unsave" : "save"}
+          </p>
+        </div>
+      </div>
       <p class="listing-details__price"></p>
       <p class="listing-details__description">{props.listing.description}</p>
       <Show when={loggedIn()} fallback={ListingDetailsContactButton}>
         <p class="listing-details__contact">
-          Contact Number: {props.listing.phone}
+          Contact number: {props.listing.phone}
         </p>
         <p class="listing-details__email">Email: {props.listing.email}</p>
       </Show>
--- src/pages/Listing/Listing.tsx
+++ src/pages/Listing/Listing.tsx
@@ -6,6 +6,7 @@ import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner"
 import { fetchListingDetails } from "../../api/api"
 import "./Listing.scss"
 import ListingImages from "../../components/ListingImages/ListingImages"
+import { savedListingsIds } from "../../store/store"
 
 const Listing: Component = () => {
   const params = useParams()
@@ -17,7 +18,10 @@ const Listing: Component = () => {
         <p class="listing__title">{listing().listing.title}</p>
         <ImageContainer images={listing().images} page="listing" />
         <ListingImages images={listing().images} />
-        <ListingDetails listing={listing().listing} />
+        <ListingDetails
+          listing={listing().listing}
+          saved={savedListingsIds().includes(listing().listing.id)}
+        />
       </Show>
     </div>
   )
